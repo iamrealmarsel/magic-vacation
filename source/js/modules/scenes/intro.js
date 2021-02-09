8fc677c155425c9bcafd0173d8abcbb118523013
mars <maspam@yandex.ru>
2021-02-10 02:02:52 +0300

    Подключил кастомный шейдер

--- source/js/modules/scenes/intro.js
+++ source/js/modules/scenes/intro.js
@@ -12,8 +12,40 @@ export default () => {
     texture.magFilter = THREE.LinearFilter;
     texture.minFilter = THREE.LinearFilter;
 
-    const material = new THREE.MeshBasicMaterial({
-      map: texture,
+    const material = new THREE.RawShaderMaterial({
+      uniforms: {
+        map: {
+          value: texture
+        }
+      },
+
+      vertexShader: `
+      // Переменные, которые передаёт Three.js для проецирования на плоскость
+      uniform mat4 projectionMatrix;
+      uniform mat4 modelMatrix;
+      uniform mat4 viewMatrix;
+
+      // Атрибуты вершины из геометрии
+      attribute vec3 position;
+      attribute vec3 normal;
+      attribute vec2 uv;
+
+      // Varying-переменная для передачи uv во фрагментный шейдер
+      varying vec2 vUv;
+
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
+      }`,
+
+      fragmentShader: `
+      precision mediump float;
+      uniform sampler2D map;
+      varying vec2 vUv;
+      void main() {
+        vec4 texel = texture2D( map, vUv );
+        gl_FragColor = texel;
+      }`,
     });
 
     let geoWidth = 1440;
--- source/js/modules/scenes/story.js
+++ source/js/modules/scenes/story.js
@@ -25,9 +25,43 @@ let geoHeight = 1024;
 
 export const loadStory = () => {
   manager.onLoad = () => {
-    textures.forEach((item, index) => {
-      const material = new THREE.MeshBasicMaterial({
-        map: item,
+    textures.forEach((texture, index) => {
+      const material = new THREE.RawShaderMaterial({
+        uniforms: {
+          map: {
+            value: texture
+          }
+        },
+
+        vertexShader: `
+          // Переменные, которые передаёт Three.js для проецирования на плоскость
+          uniform mat4 projectionMatrix;
+          uniform mat4 modelMatrix;
+          uniform mat4 viewMatrix;
+
+          // Атрибуты вершины из геометрии
+          attribute vec3 position;
+          attribute vec3 normal;
+          attribute vec2 uv;
+
+          // Varying-переменная для передачи uv во фрагментный шейдер
+          varying vec2 vUv;
+
+          void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
+          }
+        `,
+
+        fragmentShader: `
+          precision mediump float;
+          uniform sampler2D map;
+          varying vec2 vUv;
+          void main() {
+            vec4 texel = texture2D( map, vUv );
+            gl_FragColor = texel;
+          }
+        `,
       });
 
       const aspectCanvas = windowWidth / windowHeight;
